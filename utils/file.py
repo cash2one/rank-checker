d857c8cd4c641009ef4b27dd00e5e909d4d3c0c7
jlfme <jlfgeek@gmail.com>
2014-02-14 00:12:36 +0800

    Add ExcelWriter class

--- utils/file.py
+++ utils/file.py
@@ -6,6 +6,9 @@
 # ---------------------------------------
 
 
+from openpyxl.workbook import Workbook
+
+
 def remove_utf8_bom(file_path):
     """去除文件头部的UTF-8 BOM"""
 
@@ -17,3 +20,34 @@ def remove_utf8_bom(file_path):
             body = f.read()
             with open(file_path, 'wb') as wf:
                 wf.write(body)
+
+
+class ExcelWriter(object):
+    """ 创建Excel文件的工具类
+
+    Attributes:
+        filename: 文件名称,保存文件的时候使用
+        title: 标题
+    """
+
+    def __init__(self, filename, title):
+
+        self.filename = filename
+        self.work = Workbook()
+        self.chart = self.work.create_sheet()
+        self.chart.title = title
+
+    def set_column_width(self, column_dict):
+        if not isinstance(column_dict, dict):
+            raise TypeError('column_dict must be a dictionary')
+        for key, value in column_dict.items():
+            self.chart.column_dimensions[key].width = column_dict[key]
+
+    def add_content_cell(self, cell, value):
+        self.chart[cell].value = value
+
+    def add_content(self, value=list()):
+        self.chart.append(value)
+
+    def save(self):
+        self.work.save(self.filename + '.xlsx')
