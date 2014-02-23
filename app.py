a3eed52e725210ee4ce4d430cad0c5a01867cf98
jlfme <jlfgeek@gmail.com>
2014-02-23 23:08:47 +0800

    Add app BASE_DIR

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ encoding: utf-8 _*_
+# ---------------------------------------
+# Created by: Jlfme<jlfgeek@gmail.com>
+# Created on: 2014-02-23 22:59:18
+# ---------------------------------------
+
+
+import os
+
+
+BASE_DIR = os.path.dirname(__file__)
